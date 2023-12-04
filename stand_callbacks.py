d7c2c308d6a71885bc8835a928494c7c57e805a0
Aleksey <fasthydra@gmail.com>
2023-12-04 13:24:19 +0300

    Print protocol on callbacks

--- stand_callbacks.py
+++ stand_callbacks.py
@@ -41,16 +41,27 @@ scheduler_params = {
 def example_callback(stage, context, **kwargs):
     match stage:
         case 'before_run':
-            print(f"Before run: model={kwargs['model']}, loss_fn={kwargs['loss_fn']}, lrate={kwargs['lrate']}, batch_size={kwargs['batch_size']}")
+            print(f"*** model: {kwargs['model']}, loss_fn: {kwargs['loss_fn']}, start_lr: {kwargs['lrate']}, batch_size: {kwargs['batch_size']}")
         case 'after_run':
-            print(f"After run: model={kwargs['model']}, loss_fn={kwargs['loss_fn']}, lrate={kwargs['lrate']}, batch_size={kwargs['batch_size']}, history={kwargs['history']}")
-        case 'start_run':
-            print("Starting a new run...")
-        case 'end_run':
-            print("Run ended.")
+            metrics = kwargs['history'][-1]
+            start_time = context.exec_context['before_run']['timestamp']
+            end_time = context.exec_context['after_run']['timestamp']
+            epoch_duration = end_time - start_time
+            print(f"finish: lr: {metrics['lr']:.8f} train: {metrics['train_loss']:.4f}, valid: {metrics['val_loss']:.4f}, {epoch_duration:.2f} s\n")
+        case 'start_run' | 'end_run' | 'start_train' | 'end_train':
+            pass
+        case 'start_epoch':
+            end_epoch = str(kwargs['end_epoch'])
+            beg_epoch = str(kwargs['epoch']).zfill(len(end_epoch))
+            print(f"  {beg_epoch} / {end_epoch}", end="")
+        case 'end_epoch':
+            metrics = kwargs["epoch_metrics"]
+            start_time = context.exec_context['start_epoch']['timestamp']
+            end_time = context.exec_context['end_epoch']['timestamp']
+            epoch_duration = end_time - start_time
+            print(f" - lr: {metrics['lr']:.8f} train: {metrics['train_loss']:.4f}, valid: {metrics['val_loss']:.4f}, {epoch_duration:.2f} s")
         case _:
             print(f"Unknown stage: {stage}. Context: {context}, Arguments: {kwargs}")
-
 
 
 # Инициализация класса TrainStand
