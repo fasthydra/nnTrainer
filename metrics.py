ea9c11d60da2fbecc90cc2c00ae4fbe1f0ba76e8
Aleksey <fasthydra@gmail.com>
2024-01-13 16:19:41 +0300

    integrate MetricsLogger to ModelTrainer

--- metrics.py
+++ metrics.py
@@ -1,6 +1,7 @@
 import time
+import copy
 import torch
-from typing import Callable, Dict, List, Optional
+from typing import Callable, Dict, List, Optional, Union
 
 
 class MetricsLogger:
@@ -9,15 +10,19 @@ class MetricsLogger:
         Инициализация MetricsLogger.
         """
         self.metrics: Dict[str, List[Dict]] = {"training": [], "validation": [], "testing": []}
-        self.epoch_metrics: Dict[str, Dict[str, float]] = {"training": {}, "validation": {}, "testing": {}}
+        self.last_epoch_metrics: Dict[str, Dict[str, float]] = {"training": {}, "validation": {}, "testing": {}}
         self.batch_metrics: List[Dict] = []
-        self.metric_functions: Dict[str, Callable] = {}
-        self.epoch_metric_functions: Dict[str, Callable] = {}
         self.total_metrics: Dict[str, float] = {}
-        self.processed_data: int = 0
-        self.batch_sizes: List[int] = []
-        self.epoch_start_time: float = 0.0
-        self.batch_start_time: float = 0.0
+        
+        self.batch_metric_functions: Dict[str, Callable] = {}
+        self.total_metric_functions: Dict[str, Callable] = {}
+
+        self._current_epoch = None
+        
+        self._processed_data: int = 0
+        self._batch_sizes: List[int] = []
+        self._epoch_start_time: float = 0.0
+        self._batch_start_time: float = 0.0
 
     def add_metric_function(self, name: str, func: Callable, metric_type: str = "batch"):
         """
@@ -28,9 +33,9 @@ class MetricsLogger:
         :param metric_type: Тип метрики ('batch' или 'epoch').
         """
         if metric_type == "batch":
-            self.metric_functions[name] = func
+            self.batch_metric_functions[name] = func
         elif metric_type == "epoch":
-            self.epoch_metric_functions[name] = func
+            self.total_metric_functions[name] = func
 
         self.total_metrics[name] = 0.0
 
@@ -42,29 +47,33 @@ class MetricsLogger:
         :param mode: Режим эпохи ('training', 'validation', 'testing').
         """
         epoch_metrics = {}
-        for name, func in self.epoch_metric_functions.items():
+        for name, func in self.total_metric_functions.items():
             epoch_metric_value = func(data_loader)
             epoch_metrics[name] = epoch_metric_value
 
-        self.epoch_metrics[mode] = epoch_metrics
+        self.last_epoch_metrics[mode].update(epoch_metrics)
 
-    def start_epoch(self):
+    def start_epoch(self, epoch: int):
         """
         Подготавливает logger к новой эпохе, сбрасывая необходимые данные.
         """
-        self.epoch_start_time = time.time()
+        self._epoch_start_time = time.time()
+        self._current_epoch = epoch
+        self._processed_data = 0
+        self._batch_sizes = []
+
         self.batch_metrics = []
-        self.total_metrics = {name: 0.0 for name in self.metric_functions}
-        self.processed_data = 0
-        self.batch_sizes = []
+        self.total_metrics = {name: 0.0 for name in self.total_metric_functions}
+        self.last_epoch_metrics = {"epoch": self._current_epoch}
 
     def start_batch(self):
         """
         Фиксирует время начала обработки батча.
         """
-        self.batch_start_time = time.time()
+        self._batch_start_time = time.time()
 
-    def end_batch(self, outputs: torch.Tensor, labels: torch.Tensor, loss_value: float,
+    def end_batch(self, outputs: torch.Tensor, labels: torch.Tensor,
+                  loss: Union[torch.Tensor, float],
                   batch_size: Optional[int] = None):
         """
         Обновляет метрики после обработки батча и фиксирует его продолжительность.
@@ -73,31 +82,37 @@ class MetricsLogger:
         :param outputs: Выходные данные модели для батча (тензор).
         :param labels: Истинные метки для батча (тензор).
         :param batch_size: Размер батча. Если None, размер батча вычисляется из outputs.
-        :param loss_value: Значение функции потерь для батча.
+        :param loss: Значение потерь для батча, может быть тензором или float.
         """
-        batch_duration = time.time() - self.batch_start_time
+        batch_duration = time.time() - self._batch_start_time
 
         if batch_size is None:
             batch_size = outputs.size(0)
 
-        self.processed_data += batch_size
+        self._processed_data += batch_size
 
         batch_metrics = {
             "batch_duration": batch_duration,
             "metrics": {},
-            "loss": loss_value,
+            "loss": 0.0,
             "batch_size": batch_size,
-            "processed_data": self.processed_data,
+            "processed_data": self._processed_data,
         }
 
-        with torch.no_grad():
-            batch_metrics["metrics"]["loss"] = loss_value
+        if torch.is_tensor(loss) and loss.nelement() == 1:
+            batch_metrics["loss"] = loss.item()
+        elif isinstance(loss, float):
+            batch_metrics["loss"] = loss
+        else:
+            raise ValueError("Loss must be a single-element tensor or a float")
 
-            for name, func in self.metric_functions.items():
+        with torch.no_grad():
+
+            for name, func in self.batch_metric_functions.items():
                 metric_value = func(outputs, labels)
                 batch_metrics["metrics"][name] = metric_value
 
-            proc_data = 1 if self.processed_data == 0 else self.processed_data
+            proc_data = 1 if self._processed_data == 0 else self._processed_data
 
             for key in batch_metrics["metrics"].keys():
                 self.total_metrics[key] += batch_metrics["metrics"][key] * batch_size
@@ -111,26 +126,22 @@ class MetricsLogger:
 
         :param mode: Режим эпохи ('training', 'validation', 'testing').
         """
-        epoch_duration = time.time() - self.epoch_start_time
         epoch_metrics = {
-            "epoch_duration": epoch_duration,
-            "average_metrics": {},
-            "total_metrics": {},
-            "batch_metrics": self.batch_metrics
+            "duration": time.time() - self._epoch_start_time,
+            "batches": copy.deepcopy(self.batch_metrics),
+            "total": {}
         }
 
         # Вычисление средних значений метрик за эпоху
         for key, total in self.total_metrics.items():
-            if self.processed_data > 0:
-                epoch_metrics["epoch_metrics"][key] = total / self.processed_data
-            epoch_metrics["total_metrics"][key] = total
+            if self._processed_data > 0:
+                epoch_metrics[key] = total / self._processed_data
+            epoch_metrics["total"][key] = total
+
+        self.last_epoch_metrics[mode].update(epoch_metrics)
 
         # Добавление финализированных метрик в историю эпох
-        self.metrics[mode].append(epoch_metrics)
-
-        # Сброс временных данных для следующей эпохи
-        self.reset()
-
+        self.metrics[mode].append(self.last_epoch_metrics)
 
 ''' 
 ToDo:
--- trainer.py
+++ trainer.py
@@ -4,6 +4,7 @@ import time
 import torch
 from typing import Any, Dict, List, Tuple, Optional, Callable, Union
 from storage import TrainingProgressStorage
+from metrics import MetricsLogger
 
 
 class ModelTrainer:
@@ -16,6 +17,7 @@ class ModelTrainer:
         score_function=None,
         device: Optional[torch.device] = None,
         storage: Optional[TrainingProgressStorage] = None,
+        metrics_logger: Optional[MetricsLogger] = None,
         history: Optional[List[Dict[str, Any]]] = None,
         callbacks: Optional[List[Callable]] = None,
         save_every_k_epochs: int = 1
@@ -26,6 +28,7 @@ class ModelTrainer:
         self.criterion = criterion
         self.score_function = score_function if score_function else self._dummy_score_function
         self.device = device if device else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.metrics_logger = metrics_logger if metrics_logger else MetricsLogger()
         self.history = copy.deepcopy(history) if history is not None else []
         self.callbacks = callbacks if callbacks else []
 
@@ -34,6 +37,8 @@ class ModelTrainer:
         self.save_every_k_epochs = save_every_k_epochs
 
         self.first_epoch = 1
+        self._early_stop_best_val = float('inf')
+        self._early_stop_epochs_without_improvement = 0
 
         self.exec_context = {}  # данные контекста выполнения для использования в callbacks
 
@@ -85,26 +90,16 @@ class ModelTrainer:
     def get_loss(outputs, labels, loss_fn):
         return loss_fn(outputs, labels)
 
-    @staticmethod
-    def get_metrics(outputs, labels):
-        return dict()
-
     def fit_eval_epoch(self, data_loader, mode='train') -> list:
         if mode == 'train':
             self.model.train()
         else:
             self.model.eval()
 
-        running_loss = 0.0
-        processed_data = 0
-        processing_time = 0.0
-
-        epoch_metrics = []
-
         for step, data in enumerate(data_loader):
-            batch_metrics = {}
-            batch_start_time = time.time()
-            
+
+            self.metrics_logger.start_batch()
+
             inputs = self.get_inputs(data)
             labels = self.get_labels(data)
 
@@ -116,40 +111,49 @@ class ModelTrainer:
             with torch.set_grad_enabled(mode == 'train'):
                 outputs = self.get_outputs(self.model, inputs)
                 loss = self.get_loss(self.criterion, outputs, labels)
-                metrics = self.get_metrics(outputs, labels)
 
             if mode == 'train':
                 loss.backward()
                 self.optimizer.step()
 
-            running_loss += loss.item() * inputs.size(0)
-            processed_data += inputs.size(0)
-            batch_end_time = time.time()
-            batch_processing_time = batch_end_time - batch_start_time
-            processing_time += batch_processing_time
+            self.metrics_logger.end_batch(outputs, labels, loss.item())
 
-            batch_metrics["step"] = step
-            batch_metrics["batch_loss"] = loss
-            batch_metrics["batch_size"] = inputs.size(0)
-            batch_metrics["batch_start_time"] = batch_start_time
-            batch_metrics["batch_end_time"] = batch_end_time
-            batch_metrics["batch_processing_time"] = batch_processing_time
-            batch_metrics["processing_time"] = processing_time
-            batch_metrics["loss"] = running_loss / processed_data
-            batch_metrics.update(metrics)
+            self._call_callbacks('end_batch', metrics=self.metrics_logger.batch_metrics)
 
-            self._call_callbacks('end_batch', metrics=batch_metrics)
+    def fit(self, train_loader: torch.utils.data.DataLoader) -> None:
+        self.fit_eval_epoch(train_loader, mode='train')
+        self.metrics_logger.calculate_epoch_metrics(train_loader, "training")
 
-            epoch_metrics.append(batch_metrics)
-
-        return epoch_metrics
-
-    def fit(self, train_loader: torch.utils.data.DataLoader) -> float:
-        return self.fit_eval_epoch(train_loader, mode='train')
-
-    def eval(self, val_loader: torch.utils.data.DataLoader) -> float:
+    def eval(self, val_loader: torch.utils.data.DataLoader, mode: str = "validation") -> None:
         with torch.no_grad():
-            return self.fit_eval_epoch(val_loader, mode='eval')
+            self.fit_eval_epoch(val_loader, mode='eval')
+            self.metrics_logger.calculate_epoch_metrics(val_loader, mode)
+
+    def get_early_stop_value(self) -> float:
+        return self.metrics_logger.metrics["validation"][-1]["loss"]
+
+    def early_stop(self, patience) -> Union[None, bool]:
+
+        if patience == -1:
+            self._early_stop_best_val = float('inf')
+            self._early_stop_epochs_without_improvement = 0
+            return
+
+        if not patience:
+            return False
+
+        early_stop_value = self.get_early_stop_value()
+
+        if early_stop_value < self._early_stop_best_val:
+            self._early_stop_best_val = early_stop_value
+            self._early_stop_epochs_without_improvement = 0
+        else:
+            self._early_stop_epochs_without_improvement += 1
+
+        if self._early_stop_epochs_without_improvement >= patience:
+            return True
+
+        return False
 
     def train(
         self,
@@ -158,54 +162,28 @@ class ModelTrainer:
         epochs: Union[int, Tuple[int, int]],
         test_loader: Optional[torch.utils.data.DataLoader] = None,
         patience: Optional[int] = None
-    ) -> List[Dict[str, Any]]:
+    ) -> None:
 
         self._call_callbacks('start_train')
-        best_val_loss = float('inf')
-        epochs_without_improvement = 0
 
-        if isinstance(epochs, int):
-            start_epoch = self.first_epoch
-            end_epoch = epochs
-        else:
-            start_epoch, end_epoch = epochs
+        self.early_stop(-1)
+
+        start_epoch, end_epoch = self.first_epoch, epochs if isinstance(epochs, int) else epochs
 
         for epoch in range(start_epoch, end_epoch + 1):
             self._call_callbacks('start_epoch', epoch=epoch, end_epoch=end_epoch)
 
-            epoch_metrics = {}
-
-            train_loss = self.fit(train_loader)
-            train_score = self.score_function(self.model, train_loader)
-
-            epoch_metrics['lr'] = self.optimizer.param_groups[0]['lr']
-            epoch_metrics['train_loss'] = train_loss
-            epoch_metrics['train_score'] = train_score
-
-            val_loss = self.eval(val_loader)
-            val_score = self.score_function(self.model, val_loader)
-            epoch_metrics['val_loss'] = val_loss
-            epoch_metrics['val_score'] = val_score
+            self.fit(train_loader)
+            self.eval(val_loader)
 
             if test_loader:
-                test_loss = self.eval(test_loader)
-                test_score = self.score_function(self.model, test_loader)
-                epoch_metrics['test_loss'] = test_loss,
-                epoch_metrics['test_score'] = test_score
-
-            self.history.append(epoch_metrics)
+                self.eval(test_loader, mode="testing")
 
             self.save_progress(epoch)
 
             self._call_callbacks('end_epoch', epoch=epoch, end_epoch=end_epoch, epoch_metrics=epoch_metrics)
 
-            if val_loss < best_val_loss:
-                best_val_loss = val_loss
-                epochs_without_improvement = 0
-            else:
-                epochs_without_improvement += 1
-
-            if patience is not None and epochs_without_improvement >= patience:
+            if self.early_stop(patience):
                 print(f"Ранняя остановка на эпохе {epoch}.")
                 break
 
@@ -213,8 +191,6 @@ class ModelTrainer:
                 self.scheduler.step()
 
         self._call_callbacks('end_train')
-
-        return self.history
 
     def restore(self):
         last_epoch = self.first_epoch
